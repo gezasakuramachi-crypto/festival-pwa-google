05cdf2a1bf90db2101344651ab9d35e9aee912cb
gezasakuramachi-crypto <geza.sakuramachi@gmail.com>
2025-08-21 20:29:09 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+window.APP_CONFIG = {
+  googleMapsApiKey: "YOUR_GOOGLE_MAPS_API_KEY",
+  map: {
+    center: [35.966722, 140.629711],
+    zoom: 15
+  },
+  traccar: {
+    baseUrl: "https://YOUR_TRACCAR_DOMAIN",
+    deviceId: 0,
+    apiKey: "YOUR_TRACCAR_API_KEY"
+  },
+  pollMs: 8000,
+  festivalWindow: {
+    start: "2025-08-31T17:00:00+09:00",
+    end:   "2025-09-02T22:00:00+09:00"
+  }
+};
