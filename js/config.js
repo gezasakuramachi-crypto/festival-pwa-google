edfbb6da1af6d12b85f905bf18703a2c75f7aad4
gezasakuramachi-crypto <geza.sakuramachi@gmail.com>
2025-08-22 14:21:20 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 window.APP_CONFIG = {
-  googleMapsApiKey: "YOUR_GOOGLE_MAPS_API_KEY",
+  googleMapsApiKey: "AIzaSyCExrqcE4MPmievjTlV8wFJrVtKcbKWqX8",
   map: {
     center: [35.966722, 140.629711],
     zoom: 15
   },
   traccar: {
-    baseUrl: "https://YOUR_TRACCAR_DOMAIN",
-    deviceId: 0,
-    apiKey: "YOUR_TRACCAR_API_KEY"
+    baseUrl: "https://traccar-railway.fly.dev:5055",
+    deviceId: 1,
+    apiKey: "RjBEAiAVXri0u0j9x6cPqP9TXAbZDprtfJSlwudh4dE-Q82aQQIgHI_E4p-tAr9KJpNVmbcClYmVfh0Rr-VHphOP95TaMZt7InUiOjEsImUiOiIyMDI1LTA4LTI4VDE1OjAwOjAwLjAwMCswMDowMCJ9"
   },
   pollMs: 8000,
   festivalWindow: {
